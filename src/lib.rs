d98749a0cab759c72fba06d1125a6bc3dfcee246
IceTap <samcfgreene@gmail.com>
2023-04-25 21:50:42 -0700

    circle and fixed distortion BIG COMMIT

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@
 //! 
 //! 
  
-//! # Examples
+//! # Example
 //! 
 //! ```
 //! 
@@ -25,10 +25,9 @@
 //! ```
 
 
-
-
 #![allow(dead_code)]
 use glium::{glutin, DrawParameters};
+use std::f32::consts::PI;
 use std::time::{Duration, Instant};
 use glium::glutin::event_loop::{EventLoop, ControlFlow};
 use glium::glutin::event::{Event, StartCause};
@@ -42,14 +41,6 @@ pub enum Action {
 
 static mut ASPECT_RATIO: f32 = 1.0;
 
-/// # Examples
-/// 
-/// ```
-/// blithaven::start_loop(app.event_loop, move | _events | {     
-///     app.scene.save_frame((0.2,0.2,0.2)).finish().unwrap();
-///     Action::Continue
-/// });
-/// ```
 pub fn start_loop<F>(event_loop: EventLoop<()>, mut callback: F)->! where F: 'static + FnMut(&Vec<Event<'_, ()>>) -> Action {
     let mut events_buffer = Vec::new();
     let mut next_frame_time = Instant::now();
@@ -136,38 +127,24 @@ use glium::Program;
 use glium::Frame;
 use std::vec::Vec;
 
-/// A struct representing a shape in a 2D space.
 pub struct Shape {
-    position: [f32; 2],
     vertex_buffer: VertexBuffer<Vertex>,
     index_buffer: IndexBuffer<u16>,
     program: Program,
 }
 
 impl Shape {
-    /// Returns the uniforms used for rendering the shape.
-    ///
-    /// The uniforms consist of a matrix used for transforming the shape, which includes
-    /// the aspect ratio, position, and other parameters.
     fn get_uniforms(&self) -> UniformsStorage<'static, [[f32; 4]; 4], EmptyUniforms> {
         return uniform! {
             matrix: [
                 [unsafe { ASPECT_RATIO }, 0.0, 0.0, 0.0],
                 [0.0, 1.0, 0.0, 0.0],
                 [0.0, 0.0, 1.0, 0.0],
-                [self.position[0], self.position[1], -1.0, 1.0f32],
+                [0.0, 0.0, -1.0, 1.0f32],
             ]
         };
     }
-
-    /// Creates a new shape with the given parameters.
-    ///
-    /// The shape is created with a position, a list of vertices, a display object, and a color.
-    ///
-    /// # Panics
-    ///
-    /// This function will panic if the number of vertices is less than 3.
-    fn new_shape(position: [f32; 2], vertices: Vec<[f32; 2]>, display: &Display, color: (f32, f32, f32, f32)) -> Self {
+    fn new_shape(vertices: Vec<[f32; 2]>, display: &Display, color: (f32, f32, f32, f32)) -> Self {
         assert!(vertices.len() > 2, "Number of vertices must be greater than 2.");
         let mut verts = Vec::<f32>::new();
         let vertices = {
@@ -221,7 +198,7 @@ impl Shape {
             ).unwrap()
         };
         
-        Shape { position: position, vertex_buffer: vertices, index_buffer: indices, program: program }
+        Shape { vertex_buffer: vertices, index_buffer: indices, program: program }
     }
 
     fn draw(&self, frame: &mut Frame) {
@@ -235,32 +212,14 @@ pub struct Scene {
 }
 
 impl Scene {
-    /// Adds an actor with the specified shape to the scene.
-    ///
-    /// # Arguments
-    ///
-    /// * `shape` - The shape of the actor to be added to the scene.
-    ///
-    ///
+
     pub fn add_actor(&mut self, shape: Shape) {
         self.actors.push(shape)
     }
 
-    /// Saves the current frame of the scene to a `Frame` object.
-    ///
-    /// # Arguments
-    ///
-    /// * `clear_color` - The color to be used for clearing the frame's color and depth buffers.
-    ///                   It should be specified as a tuple of four f32 values representing RGBA.
-    ///
-    /// # Returns
-    ///
-    /// The `Frame` object representing the saved frame.
-    ///
-    ///
     pub fn save_frame(&mut self, clear_color: (f32, f32, f32)) -> Frame {
         let mut frame = self.display.draw();
-        frame.clear_color_and_depth((clear_color.0, clear_color.1, clear_color.2, 1.0), 1.0);
+        frame.clear_color_and_depth((clear_color.0, clear_color.1, clear_color.2, 0.5), 1.0);
         for actor in self.actors.iter() {
             actor.draw(&mut frame);
         }
@@ -268,66 +227,30 @@ impl Scene {
         frame
     }
 
-    /// Draws a polygon shape on the scene.
-    ///
-    /// # Arguments
-    ///
-    /// * `vertices` - The vertices of the polygon shape. Should be specified as a vector of arrays
-    ///                 with two f32 values representing the x and y coordinates of each vertex.
-    /// * `color` - The color of the polygon shape. Should be specified as a tuple of four f32 values
-    ///             representing RGBA.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// let mut app = App::init("Example Polygon");
-    /// 
-    /// let vertices = vec![[0.0, 0.0], [1.0, 1.0], [-1.0, 1.0]];
-    /// app.scene.draw_polygon(vertices, (0.0, 1.0, 0.0, 1.0));
-    /// // The polygon shape with the specified vertices and color will be drawn on the scene.
-    /// ```
-    ///
     pub fn draw_polygon(&mut self, vertices: Vec<[f32; 2]>, color: (f32, f32, f32, f32)) {
-        self.add_actor(Shape::new_shape(vertices[0], vertices, &self.display, color));
+        self.add_actor(Shape::new_shape(vertices, &self.display, color));
     }
 
-    /// Add a rectangle shape to the scene.
-    ///
-    /// # Arguments
-    ///
-    /// * `position` - The position of the rectangle as a `[f32; 2]` array representing the x and y coordinates.
-    /// * `width` - The width of the rectangle as a `f32` value.
-    /// * `height` - The height of the rectangle as a `f32` value.
-    /// * `color` - The color of the rectangle as a tuple of `(f32,f32,f32,f32)` representing RGBA values.
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// let mut scene = Scene::new();
-    /// scene.draw_rect([0.0, 0.0], 100.0, 50.0, (1.0, 0.0, 0.0, 1.0));
-    /// ```
     pub fn draw_rect(&mut self, position: [f32; 2], width: f32, height: f32, color: (f32,f32,f32,f32)) {
         let vertecies = vec![position, [position[0] + width, position[1]], [position[0] + width, position[1] - height], [position[0], position[1] - height]];
-        self.add_actor(Shape::new_shape(position, vertecies, &self.display, color));
+        self.add_actor(Shape::new_shape(vertecies, &self.display, color));
     }
 
-    /// Add a square shape to the scene.
-    ///
-    /// # Arguments
-    ///
-    /// * `position` - The position of the square as a `[f32; 2]` array representing the x and y coordinates.
-    /// * `size` - The size of the square as a `f32` value.
-    /// * `color` - The color of the square as a tuple of `(f32,f32,f32,f32)` representing RGBA values.
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// let mut scene = Scene::new();
-    /// scene.draw_square([50.0, 50.0], 100.0, (0.0, 1.0, 0.0, 1.0));
-    /// ```
     pub fn draw_square(&mut self, position: [f32; 2], size: f32, color: (f32,f32,f32,f32)) {
         let vertecies = vec![position, [position[0] + size, position[1]], [position[0] + size, position[1] - size], [position[0], position[1] - size]];
-        self.add_actor(Shape::new_shape(position, vertecies, &self.display, color));
+        self.add_actor(Shape::new_shape(vertecies, &self.display, color));
+    }
+
+    pub fn draw_circle(&mut self, position: [f32; 2], radius: f32, color: (f32,f32,f32,f32)) {
+        let mut vertecies = vec![position];
+        let vertex_count = 48;
+
+        for i in 0 .. vertex_count {
+            let x = (i as f32 * PI) / 12.0;
+            vertecies.push([position[0] + (x).cos() * radius, position[1] + (x).sin() * radius]);
+        }vertecies.push([position[0] + radius, position[1]]);
+
+        self.add_actor(Shape::new_shape(vertecies, &self.display, color));
     }
     
 }
@@ -342,20 +265,32 @@ pub struct App {
 }
 
 impl App {
-    /// Initialize the app with scene and eventloop.
-    ///
-    /// # Arguments
-    ///
-    /// * `title` - The title of the created window.
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// let mut app = App::init("Example window");
-    /// ```
     pub fn init(title: &str) -> Self {
+        let mut icon: Vec<u8> = vec![];
+        let mut counter = 0;
+
+        for i in 0 .. 256 * 4 {
+            if counter == 3 {
+                icon.push(255);
+                counter = 0;
+            }
+
+            else {
+                if i % 2 == 0 {
+                    icon.push(200)
+                }
+                else if i % 3 == 0 {
+                    icon.push(150);
+                } 
+                
+                else {icon.push(60)}
+                counter += 1
+            }
+        }
+        let icon = glutin::window::Icon::from_rgba(icon, 16, 16).expect("BADICON");
+
         let event_loop = glutin::event_loop::EventLoop::new();
-        let window = glutin::window::WindowBuilder::new().with_title(title);
+        let window = glutin::window::WindowBuilder::new().with_title(title).with_window_icon(Some(icon));
         let context_buffer = glutin::ContextBuilder::new().with_depth_buffer(24);
         App {scene: Scene { actors: Vec::new(), display: glium::Display::new(window, context_buffer, &event_loop).unwrap() }, event_loop: event_loop }
     }
